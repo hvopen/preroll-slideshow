e368b7aa7b5183057c8ebcd6c9c9a853e3f73e21
Sean Dague <sean@dague.net>
2017-01-04 10:01:58 -0500

    update images

--- phantom/meetup.js
+++ phantom/meetup.js
@@ -5,8 +5,8 @@ Capture meetup site to a file
 */
 
 var page = require('webpage').create();
-page.viewportSize = { width: 1200, height: 1024 };
-page.clipRect = { top: 90, left: 0, width: 1200, height: 1024 };
+page.viewportSize = { width: 1800, height: 1024 };
+page.clipRect = { top: 290, left: 300, width: 1200, height: 1024 };
 page.open('http://www.meetup.com/mhvlug/', function() {
   page.render('images/meetup.png');
   phantom.exit();
