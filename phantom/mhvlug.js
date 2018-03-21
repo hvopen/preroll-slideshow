0210f40d8cc7537fc888fc917f8a89796ff6fffd
Sean Dague <sean@dague.net>
2018-03-21 16:08:56 -0400

    add screen shot for hvopen

--- phantom/mhvlug.js
+++ phantom/mhvlug.js
@@ -5,9 +5,9 @@ Capture meetup site to a file
 */
 
 var page = require('webpage').create();
-page.viewportSize = { width: 1800, height: 1024 };
-page.clipRect = { top: 0, left: 300, width: 1200, height: 1024 };
-page.open('https://mhvlug.org', function() {
-  page.render('images/mhvlug.png');
+page.viewportSize = { width: 1200, height: 1024 };
+page.clipRect = { top: 0, left: 0, width: 1200, height: 1024 };
+page.open('https://hvopen.org', function() {
+  page.render('images/hvopen.png');
   phantom.exit();
 });
